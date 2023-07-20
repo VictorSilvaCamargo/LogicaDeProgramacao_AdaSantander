e8b85d60d68b7659a01a9ed91cc25fc8088a6bf3
Victor S. Camargo <victorscamargo51@gmail.com>
2023-07-20 09:15:19 -0300

    Ordena numeros em ordem crescente e decrescente

---
+++ OrdenarNumeros/OrdenarNumeros.java
@@ -0,0 +1,50 @@
+package OrdenarNumeros;
+
+import java.util.Arrays;
+
+public class OrdenarNumeros {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 4, 1, 2, 3};
+        int[] ascendingOrder = sortAscendingOrder(numbers);
+        int[] descendingOrder = sortDescendingOrder(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(ascendingOrder));
+        System.out.println(Arrays.toString(descendingOrder));
+    }
+
+    public static int[] sortAscendingOrder(int[] lista) {
+        int[] listaOrdenada = Arrays.copyOf(lista, lista.length);
+
+        for (int i = 0; i < listaOrdenada.length - 1; i++) {
+            for (int j = 0; j < listaOrdenada.length - i - 1; j++) {
+                if (listaOrdenada[j] > listaOrdenada[j + 1]) {
+                    int temp = listaOrdenada[j];
+                    listaOrdenada[j] = listaOrdenada[j + 1];
+                    listaOrdenada[j + 1] = temp;
+                }
+            }
+        }
+
+        return listaOrdenada;
+    }
+
+    public static int[] sortDescendingOrder(int[] lista) {
+        int[] listaOrdenada = Arrays.copyOf(lista, lista.length);
+
+        for (int i = 0; i < listaOrdenada.length - 1; i++) {
+            for (int j = 0; j < listaOrdenada.length - i - 1; j++) {
+                if (listaOrdenada[j] < listaOrdenada[j + 1]) {
+                    int temp = listaOrdenada[j];
+                    listaOrdenada[j] = listaOrdenada[j + 1];
+                    listaOrdenada[j + 1] = temp;
+                }
+            }
+        }
+
+        return listaOrdenada;
+    }
+
+}
+
