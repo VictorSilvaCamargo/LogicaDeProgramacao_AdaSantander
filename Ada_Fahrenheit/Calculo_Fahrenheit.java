166ad4541a58c99107102a3ce7b8e7ff94b26c17
Victor S. Camargo <victorscamargo51@gmail.com>
2023-07-20 09:09:09 -0300

    Converte Fahrenheit para Celcius

---
+++ Ada_Fahrenheit/Calculo_Fahrenheit.java
@@ -0,0 +1,21 @@
+package Ada_Fahrenheit;
+
+import java.util.Scanner;
+
+public class Calculo_Fahrenheit {
+
+    public static double converter_Fahrenheit(double fahrenheit) {
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        return celsius;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a temperatura em Fahrenheit para converter para Celsius:");
+        double graus = scanner.nextDouble();
+        double temperaturaCelsius = converter_Fahrenheit(graus);
+        System.out.printf("Graus Fahrenheit: %.2f para graus Celsius: %.2f", graus, temperaturaCelsius);
+        scanner.close();
+    }
+}
+
