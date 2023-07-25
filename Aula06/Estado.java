a8efb1c1cd3ab8160ecb039cec18153770f9e595
Victor S. Camargo <victorscamargo51@gmail.com>
2023-07-24 21:08:04 -0300

    Aprendendo o uso do Enum

---
+++ Aula06/Estado.java
@@ -0,0 +1,17 @@
+package Aula06;
+
+enum Estado {
+    MG(7),
+    RJ(15),
+    SP(12);
+
+    private final double taxaImposto;
+
+    Estado(double taxaImposto) {
+        this.taxaImposto = taxaImposto;
+    }
+
+    public double getTaxaImposto() {
+        return taxaImposto;
+    }
+}
