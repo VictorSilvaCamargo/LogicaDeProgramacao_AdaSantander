c959c152928c346d8c0c96f83b18329558627ccf
Victor S. Camargo <victorscamargo51@gmail.com>
2023-07-25 13:40:42 -0300

    Exercícios sobre Exeptions

--- Aula05/QuantidadeException.java
+++ Aula05/QuantidadeException.java
@@ -2,6 +2,7 @@ package Aula05;
 
 public class QuantidadeException extends Exception {
     public QuantidadeException() {
+
         super("A quantidade de frutas deve ser maior que zero.");
     }
 }
